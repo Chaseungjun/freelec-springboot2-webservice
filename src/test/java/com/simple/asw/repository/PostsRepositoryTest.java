7509fd0901a8199c7d5c2ed3d46c25e36c66d658
cha <chas369@naver.com>
2022-05-05 18:42:21 +0900

    commit

--- src/test/java/com/simple/asw/repository/PostsRepositoryTest.java
+++ src/test/java/com/simple/asw/repository/PostsRepositoryTest.java
@@ -2,46 +2,69 @@ package com.simple.asw.repository;
 
 import com.simple.asw.domain.posts.Posts;
 import com.simple.asw.domain.posts.PostsRepository;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
-@Transactional
-class PostsRepositoryTest {
+public class PostsRepositoryTest {
 
     @Autowired
     PostsRepository postsRepository;
 
-    @Test
-    @DisplayName("게시글_저장")
-    void 게시글_저장() throws Exception {
-        //given
-        String title = "test";
-        String content = "content test";
-
-        //when
-        Posts posts = Posts.builder()
-                .title(title)
-                .content(content)
-                .author("aaa@aaa.aaa")
-                .build();
-        postsRepository.save(posts);
-
-        //then
-        assertEquals(posts.getTitle(), title);
-        assertEquals(posts.getContent(), content);
+    @AfterEach
+    public void cleanup() {
+        postsRepository.deleteAll();
     }
 
+    @Test
+    public void 게시글저장_불러오기() {
+        //given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
 
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("jojoldu@gmail.com")
+                .build());
 
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void BaseTimeEntity_등록() {
+        //given
+        LocalDateTime now = LocalDateTime.of(2019, 6, 4, 0, 0, 0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>>>>> createDate=" + posts.getCreateDate() + ", modifiedDate=" + posts.getModifiedDate());
+
+        assertThat(posts.getCreateDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
+    }
 }
